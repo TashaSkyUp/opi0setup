252aa50d7f761f6c1a3e662522d55a814e3a11b6
Tasha Sky Upchurch <TashaSkyUp@gmail.com>
2017-10-25 04:57:17 +0000

    bug hunting

--- editopi0.sh
+++ editopi0.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+opiFile="/boot/bin/orangepizero.bin"
+opiTestFile="/boot/bin/orangepizerotest.bin"
 echo "show dialog"
 whiptail --title "orangepizero.bin" --msgbox "save and exit"  10 25
 echo "bin->fex"
+
+if [ -e $opiTestFile ]
+then 
+bin2fex /boot/bin/orangepizerotest.bin ./temp.fex
+else
 bin2fex /boot/bin/orangepizero.bin ./temp.fex
+fi
+
 echo "nano"
 nano temp.fex
 echo "fex->bin"
-fex2bin temp.fex /boot/bin/orangepizerotest.bin
-rm temp.fex
+fex2bin ./temp.fex /boot/bin/orangepizerotest.bin
+rm ./temp.fex
 
 ln -sf /boot/bin/orangepizerotest.bin /boot/script.bin
 
