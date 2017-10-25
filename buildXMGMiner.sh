b191225c812b336f31eda6a1f2578ea77425013a
root <root@localhost.localdomain>
2017-10-25 06:22:57 -0600

    added -y

--- buildXMGMiner.sh
+++ buildXMGMiner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev -y
 git clone https://github.com/novaspirit/wolf-m7m-cpuminer
 cd wolf-m7m-cpuminer 
 $ ./autogen.sh
