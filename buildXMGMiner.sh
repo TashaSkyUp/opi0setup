207f5a2a08b8753c10a05919df1eb35a2d687b75
Tasha Sky Upchurch <TashaSkyUp@gmail.com>
2017-10-25 12:19:41 +0000

    script to compile a XMG minger

---
+++ buildXMGMiner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev
+git clone https://github.com/novaspirit/wolf-m7m-cpuminer
+cd wolf-m7m-cpuminer 
+$ ./autogen.sh
+./configure
+make -j4
