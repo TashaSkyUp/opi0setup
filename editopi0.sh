55c95deb70423b9360117c7d2146c6af8351475f
Tasha Sky Upchurch <TashaSkyUp@gmail.com>
2017-10-25 05:27:15 +0000

    bug hunting

--- editopi0.sh
+++ editopi0.sh
@@ -7,13 +7,15 @@ echo "bin->fex"
 
 if [ -e $opiTestFile ]
 then 
+echo "exists"
 bin2fex /boot/bin/orangepizerotest.bin ./temp.fex
 else
+echo "new run"
 bin2fex /boot/bin/orangepizero.bin ./temp.fex
 fi
 
 echo "nano"
-nano temp.fex
+nano ./temp.fex
 echo "fex->bin"
 fex2bin ./temp.fex /boot/bin/orangepizerotest.bin
 rm ./temp.fex
@@ -21,6 +23,6 @@ rm ./temp.fex
 ln -sf /boot/bin/orangepizerotest.bin /boot/script.bin
 
 echo "ENABLE=true
-MIN_SPEED=480000
+MIN_SPEED=912000
 MAX_SPEED=1200000
 GOVERNOR=performance" > /etc/default/cpufrequtils
