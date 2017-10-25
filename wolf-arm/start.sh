a207d8e863ae4180f4ecbde88b86aa83138b6d93
Tasha Sky Upchurch <TashaSkyUp@gmail.com>
2017-10-25 04:26:39 +0000

    script detects what machine it is based on ip

--- wolf-arm/start.sh
+++ wolf-arm/start.sh
@@ -1,1 +1,7 @@
-./minerd -S -a m7mhash -o stratum+tcp://mining.m-hash.com:3334 -u TashaSkyUp.opi00 -p !Biago123 -B
+#!/bin/bash
+ip="$(ip addr show | grep "192.168.1.7." -o)"
+echo "test $ip"
+machine="$(echo $ip| cut -d'7' -f 2)"
+echo "test $machine"
+
+./minerd -S -a m7mhash -o stratum+tcp://mining.m-hash.com:3334 -u TashaSkyUp.opi0$machine -p !Biago123 -B
