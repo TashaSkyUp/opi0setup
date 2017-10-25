cc46dff4471586ad1747a816492b5726a22d35ca
= <TashaSkyUp@gmail.com>
2017-10-25 03:54:34 +0000

    quite mining

--- wolf-arm/start.sh
+++ wolf-arm/start.sh
@@ -1,1 +1,1 @@
-./minerd -a m7mhash -o stratum+tcp://mining.m-hash.com:3334 -u TashaSkyUp.opi00 -p !Biago123 -B
+./minerd -S -a m7mhash -o stratum+tcp://mining.m-hash.com:3334 -u TashaSkyUp.opi00 -p !Biago123 -B
