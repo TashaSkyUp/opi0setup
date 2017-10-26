8ef1933057cfa8afe70b038eb461b173a01b96ca
= <TashaSkyUp@gmail.com>
2017-10-26 08:48:19 +0000

    made it easier to change max clock speed

--- editopi0.sh
+++ editopi0.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ -z "$*" ]
+then
+exit 0
+fi
 opiFile="/boot/bin/orangepizero.bin"
 opiTestFile="/boot/bin/orangepizerotest.bin"
 echo "show dialog"
@@ -14,6 +18,11 @@ echo "new run"
 bin2fex /boot/bin/orangepizero.bin ./temp.fex
 fi
 
+line=$(cat ./temp.fex | grep "cooler0 = ........" -o)
+echo $line
+last=$(echo $line | cut -d'"' -f 2)
+echo "($last)"
+sed -i "s/$last/$2/g" ./temp.fex
 echo "nano"
 nano ./temp.fex
 echo "fex->bin"
@@ -23,6 +32,6 @@ rm ./temp.fex
 ln -sf /boot/bin/orangepizerotest.bin /boot/script.bin
 
 echo "ENABLE=true
-MIN_SPEED=912000
-MAX_SPEED=1200000
+MIN_SPEED=$1
+MAX_SPEED=$2
 GOVERNOR=performance" > /etc/default/cpufrequtils
