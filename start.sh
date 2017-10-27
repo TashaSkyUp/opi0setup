13be9a5f08a88eea626e93845b813a02658911a5
Tasha Upchurch <TashaSkyUp@Gmail.com>
2017-10-27 04:03:33 +0000

    Tasha is my scripting goddess. She automated the process of checking for installing and running rpimonitor.

--- start.sh
+++ start.sh
@@ -1,3 +1,22 @@
+a=$(service rpimonitor status)
+a=$(echo "$a" | grep "not-found" -o)
+echo "-$a-"
+#exit 0
+
+if [ ${#a} -eq 0 ]
+then
+echo "is already installed"
+
 cd wolf-arm
 ./start.sh
 armbianmonitor -m
+
+else
+armbianmonitor -r
+echo "not yet installed - will install"
+cd wolf-arm
+./start.sh
+armbianmonitor -m
+
+fi
+
