b6657f583c65df9f87f15b2f1a8aaafe185bc4fe
Tasha Upchurch <TashaSkyUp@Gmail.com>
2017-10-27 01:46:01 -0600

    huh

--- start.sh
+++ start.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-a="$(lscpu | grep "x86_64")"
-b="$(lscpu | grep "armv7l")"
-if [ ${#a} -eq 0 ]
-then
-echo "not x86_64"
-else
-c="x86_64"
-fi
-if [ ${#b} -eq 0 ]
-then
-echo "not armv7l"
-else
-c="armv7l"
-fi
 
 case "$c" in
 "x86_64")
