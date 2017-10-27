852bd1fbb73f95f15f54f81de9d0b5988d04e9fe
Tasha Upchurch <TashaSkyUp@Gmail.com>
2017-10-27 00:53:28 -0600

    inprocess

--- start.sh
+++ start.sh
@@ -3,44 +3,51 @@ a="$(lscpu | grep "x86_64")"
 b="$(lscpu | grep "armv7l")"
 if [ ${#a} -eq 0 ]
 then
-
+echo "not x86_64"
 else
 c="x86_64"
 fi
 if [ ${#b} -eq 0 ]
 then
+echo "not armv7l"
 else
 c="armv7l"
 fi
+
 case "$c" in
 "x86_64")
 echo "64"
+d="wolf-amd64"
 ;;
 "armv7l")
 echo "arm"
+d="wolf-arm"
 ;;
 *)
 echo "nada"
 ;;
 esac
-exit 0
-a=$(service rpimonitor status)
-a=$(echo "$a" | grep "not-found" -o)
+
+a="$(service rpimonitor status)"
+if [ ${#a} -eq 0 ]
+then
+a="not-found"
+fi
+a="$(echo "$a" | grep "not-found" -o)"
 echo "-$a-"
-#exit 0
 
 if [ ${#a} -eq 0 ]
 then
 echo "is already installed"
 
-cd wolf-arm
+cd $d
 ./start.sh
 armbianmonitor -m
 
 else
 armbianmonitor -r
 echo "not yet installed - will install"
-cd wolf-arm
+cd $d
 ./start.sh
 armbianmonitor -m
 
