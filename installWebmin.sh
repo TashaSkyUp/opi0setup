b7a5b2ec9df31a94aea607c77b4e57369ed0ca10
TashaSkyUp <TashaSkyUp@gmail.com>
2017-10-29 06:19:27 -0600

    Create new file

---
+++ installWebmin.sh
@@ -0,0 +1,5 @@
+sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.860_all.deb
+sudo dpkg --install webmin_1.860_all.deb 
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python -y
+sudo apt-get -f install -y
+sudo dpkg --install webmin_1.860_all.deb -y
