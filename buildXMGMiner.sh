12a0b30229d068b368f45edb8e9d658ebad70c49
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-06 06:00:52 -0700

    Update buildXMGMiner.sh

--- buildXMGMiner.sh
+++ buildXMGMiner.sh
@@ -19,7 +19,19 @@ case $1 in
     apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev -y
     git clone https://github.com/magi-project/wolf-m7m-cpuminer-V2
     ;;
-  "--replace-a53")
+  "--replace-a7")
+    #needs work see readme in  wolf v2 repo
+    cd $md
+    ./autogen.sh
+    CFLAG="-O2 mfpu=neon-vfpv4" ./configure
+
+    cp  ./Makefile ./Makefile.old
+    cp  ./m7/Makefile ./m7/Makefile.old
+  
+    sed -i 's/-march=native/-mcpu=cortex-a7/g' ./Makefile
+    sed -i 's/-march=native/-mcpu=cortex-a7/g' ./m7/Makefile     
+    ;;
+   "--replace-a53")
     #needs work see readme in  wolf v2 repo
     cd $md
     ./autogen.sh
@@ -31,7 +43,6 @@ case $1 in
     sed -i 's/-march=native/-mcpu=cortex-a53/g' ./Makefile
     sed -i 's/-march=native/-mcpu=cortex-a53/g' ./m7/Makefile     
     ;;
- 
   "--build")
     case $arch in
       "x86_64")
