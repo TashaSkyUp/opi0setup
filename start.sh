17f155c452dc420f7c2bdcb5067dc299fb139ab0
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-08 01:02:44 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 echo "$(uname -m)"
 
@@ -11,7 +18,7 @@ a="not-found"
 fi
 
 function startMiner {
-./startMiner.sh $1
+$DIR/startMiner.sh $1
 armbianmonitor -m
 }
 
