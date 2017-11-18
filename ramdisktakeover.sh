8b3be1fc134e29426a356897007fd5d7d3ffacc9
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-17 18:15:54 -0700

    Update ramdisktakeover.sh

--- ramdisktakeover.sh
+++ ramdisktakeover.sh
@@ -4,22 +4,23 @@ mkdir $TARGETDIR
 mkdir $TARGETDIR/ramdisk
 mount -t ramfs -o size=256m ext4 ./$TARGETDIR/ramdisk
 mount | grep ram
-TARGETDIR="takeover/ramdisk"
-exit 0
-# Mount Kernel Virtual File Systems
 
-mount -t proc proc $TARGETDIR/proc
-mount -t sysfs sysfs $TARGETDIR/sys
-mount -t devtmpfs devtmpfs $TARGETDIR/dev
-mount -t tmpfs tmpfs $TARGETDIR/dev/shm
-mount -t devpts devpts $TARGETDIR/dev/pts
+Echo "Mount Kernel Virtual File Systems"
+  TARGETDIR="takeover/ramdisk"
 
-# Copy /etc/hosts
-/bin/cp -f /etc/hosts $TARGETDIR/etc/
+  mount -t proc proc $TARGETDIR/proc
+  mount -t sysfs sysfs $TARGETDIR/sys
+  mount -t devtmpfs devtmpfs $TARGETDIR/dev
+  mount -t tmpfs tmpfs $TARGETDIR/dev/shm
+  mount -t devpts devpts $TARGETDIR/dev/pts
+  exit 0
+  
+  # Copy /etc/hosts
+  /bin/cp -f /etc/hosts $TARGETDIR/etc/
 
-# Copy /etc/resolv.conf 
-/bin/cp -f /etc/resolv.conf $TARGETDIR/etc/resolv.conf
+  # Copy /etc/resolv.conf 
+  /bin/cp -f /etc/resolv.conf $TARGETDIR/etc/resolv.conf
 
-# Link /etc/mtab
-chroot $TARGETDIR rm /etc/mtab 2> /dev/null 
-chroot $TARGETDIR ln -s /proc/mounts /etc/mtab
+  # Link /etc/mtab
+  chroot $TARGETDIR rm /etc/mtab 2> /dev/null 
+  chroot $TARGETDIR ln -s /proc/mounts /etc/mtab
