c4ca0184809973470ab7ff1f78a8cdb72db70f51
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-17 18:37:18 -0700

    Update ramdisktakeover.sh

--- ramdisktakeover.sh
+++ ramdisktakeover.sh
@@ -26,14 +26,14 @@ echo "Mount Kernel Virtual File Systems"
   mount -t devtmpfs devtmpfs $TARGETDIR/dev
   mount -t tmpfs tmpfs $TARGETDIR/dev/shm
   mount -t devpts devpts $TARGETDIR/dev/pts
-  exit 0
+  
   
   # Copy /etc/hosts
   /bin/cp -f /etc/hosts $TARGETDIR/etc/
 
   # Copy /etc/resolv.conf 
   /bin/cp -f /etc/resolv.conf $TARGETDIR/etc/resolv.conf
-
+  exit 0
   # Link /etc/mtab
   chroot $TARGETDIR rm /etc/mtab 2> /dev/null 
   chroot $TARGETDIR ln -s /proc/mounts /etc/mtab
