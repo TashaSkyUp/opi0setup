66f7c406af7f073c381f958c273f6d149672db2c
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-22 19:08:22 -0700

    Update startMiner.sh

--- startMiner.sh
+++ startMiner.sh
@@ -1,37 +1,47 @@
+#purpose; to start the minerd script with the correct workername and using the correct arch
+
 #!/bin/bash
-maddr="104.131.5.234"
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-arch="$(uname -m)"
-minerDir="wolf$arch"
-echo "using full path: $DIR/$minerDir"
-cd $DIR/$minerDir
+#address to mining pool
+	maddr="104.131.5.234"
 
-ip="$(hostname -I)"
-case $1 in
-  "--workername")
-  ip="$2"
-  echo "using workername $ip"
-  ;;
-esac
+#find the path to this file and store it in DIR
+	SOURCE="${BASH_SOURCE[0]}"
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-case $ip in
-  "")
-    echo "no ip detected use --workername"
-    exit 0
-    ;;
-  *)
-    echo "using IP $ip"
-    machine="$arch$(echo $ip| cut -d'.' -f 4)"
-    echo "test $machine $1"
-    ;;
-esac
+#find,echo and change to path to minerd
+	arch="$(uname -m)"
+	minerDir="wolf$arch"
+	echo "using full path: $DIR/$minerDir"
+	cd $DIR/$minerDir
+	
+#use IP or workername as part of pool workername paramater for minerd
+	ip="$(hostname -I)"
+	case $1 in
+	  "--workername")
+	  ip="$2"
+	  echo "using workername $ip"
+	  ;;
+	esac
+
+	case $ip in
+	  "")
+	    echo "no ip detected use --workername"
+	    exit 0
+	    ;;
+	  *)
+	    echo "using IP $ip"
+	    machine="$arch$(echo $ip| cut -d' ' -f 2)"
+	    machine="$arch$(echo $machine| cut -d'.' -f 4)"
+	    
+	    echo "test $machine $1"
+	    ;;
+	esac
 
 case $1 in
 "--benchmark")
