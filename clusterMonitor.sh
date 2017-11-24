ebc4fc874ce6e8fe5860533f6bf02491d3d75c6c
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-24 01:36:25 -0700

    Update clusterMonitor.sh

--- clusterMonitor.sh
+++ clusterMonitor.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl 172.24.1.1:1880/opi0cluster?register=1
+$(curl 172.24.1.1:1880/opi0cluster?register=1)
+clusterNumber="$(curl 172.24.1.1:1880/opi0cluster?register=1 | cut -d',' -f 2 | cut -d':' -f2 | grep -o "[0-9]*")" 
+echo "$clusterNumber" > /clusterNumber
