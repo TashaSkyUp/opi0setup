644b6edcb8aa55d3fb415677b14d35ff640e7ed9
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-24 20:08:21 -0700

    Update clusterMonitor.sh

--- clusterMonitor.sh
+++ clusterMonitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mac="$(hostname -i| grep "eth0.*%" -o | cut -d' ' -f 2 | cut -d'%' -f 1)"
-while 1 
+while 1 ; do
   clusterNumber="$(curl 172.24.1.1:1880/opi0cluster?register="$mac" | cut -d',' -f 2 | cut -d':' -f2 | grep -o "[0-9]*")" 
   echo "$clusterNumber" > /clusterNumber
   sleep 10
