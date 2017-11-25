cff2a93088b9821806d14253a6724f92328b7eef
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-24 18:35:42 -0700

    Update editopi0.sh

--- editopi0.sh
+++ editopi0.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 if [ -z "$*" ] #if no arguments exit
+
 then
-exit
+	exit
 fi
-=======
->>>>>>> e36e93b764549dde7cf4d94ea917fd48c96ff93d
+
 opiFile="/boot/bin/orangepizero.bin"
 opiTestFile="/boot/bin/orangepizerotest.bin"
 opiTmpFile="./temp.fex"
 opiScriptBinFile="/boot/script.bin"
 opiUseFile=$opiTestFile
 
-a="$(command -v bin2fex)"
-echo "$a"
-echo "${#a}"
-if [ ${#a} -le 0 ]
-then 
-	echo "error"
-	exit 0
-else 
-	echo "bin2fex installed"
-fi
+# check if bin2fex is installed
+	a="$(command -v bin2fex)"  
+	echo "$a"
+	echo "${#a}"
+	if [ ${#a} -le 0 ]
+	then 
+		echo "error"
+		exit 0
+	else 
+		echo "bin2fex installed"
+	fi
 
 function updateTmpFile {
 	line=$(cat $opiTmpFile | grep "cooler0 = ........" -o)
@@ -40,44 +40,44 @@ function fexToBin {
 }
 
 function updateMinMaxFile {
-echo "ENABLE=true
-MIN_SPEED=$1
-MAX_SPEED=$2
-GOVERNOR=performance" > /etc/default/cpufrequtils
+	echo "ENABLE=true
+	MIN_SPEED=$1
+	MAX_SPEED=$2
+	GOVERNOR=performance" > /etc/default/cpufrequtils
 }
 
 echo "show dialog"
 whiptail --title "orangepizero.bin" --msgbox "save and exit"  10 25
 
-echo "bin->fex"
-if [ -e $opiTestFile ]
-then 
-echo "exists"
-bin2fex $opiTestFile $opiTmpFile
-else
-echo "new run"
-bin2fex $opiFile $opiTmpFile
-fi
+#conver bin file to tmp fex file
+	echo "bin->fex"
+	if [ -e $opiTestFile ];then 
+		echo "exists"
+		bin2fex $opiTestFile $opiTmpFile
+	else
+		echo "new run"
+		bin2fex $opiFile $opiTmpFile
+	fi
 
 case $1 in
-"--revert")
-	echo  "revert to base config"
-;;
-"")
-	echo "no params - please edit"
-	echo "nano"
-	nano $opiTmpFile
-	nano /etc/default/cpufrequtils
-	fexToBin
-;;
-*)
-	echo "auto updating "
-	$global="testG"
-	fexToBin $global
-	updateMinMaxFile $1 $2
-	updateTmpFile $2
+	"--revert")
+		echo  "revert to base config"
+	;;
+	"")
+		echo "no params - please edit"
+		echo "nano"
+		nano $opiTmpFile
+		nano /etc/default/cpufrequtils
+		fexToBin
+	;;
+	*)
+		echo "auto updating "
+		$global="testG"
+		fexToBin $global
+		updateMinMaxFile $1 $2
+		updateTmpFile $2
 
-;;
+	;;
 esac
 exit 0
 
