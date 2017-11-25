b941b66ab8cfc306e9c2256e537dd0d298fb38ee
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-24 21:51:03 -0700

    Update clusterMonitor.sh

--- clusterMonitor.sh
+++ clusterMonitor.sh
@@ -11,5 +11,25 @@
 while [ . ]; do
   clusterNumber="$(curl 172.24.1.1:1880/opi0cluster?register="$mac" | cut -d',' -f 2 | cut -d':' -f2 | grep -o "[0-9]*")" 
   echo "$clusterNumber" > /clusterNumber
+  
+  result="$(ps all | grep '..:[0-9][1-9] ./minerd' -o)"
+  case "$result" in
+    
+    #is not running
+    "")
+      echo 0 >/sys/class/leds/red_led/brightness
+      echo "zero length"
+      ;;
+    
+    #is running
+    *)
+      #echo "non zero len"
+      echo 255 >/sys/class/leds/red_led/brightness
+      ./start.sh --workername $clusterNumber > /mining.log &
+      ;;
+
+  esac
+
   sleep 10
+
 done
