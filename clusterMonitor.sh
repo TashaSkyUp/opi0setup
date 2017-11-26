9df7969b7cbd8a4c99aaa2689890427a199ec1ab
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-25 22:31:27 -0700

    Update clusterMonitor.sh

--- clusterMonitor.sh
+++ clusterMonitor.sh
@@ -23,27 +23,31 @@ case $mac in
 esac
 
 while [ . ]; do
-  clusterNumber="$(curl 172.24.1.1:1880/opi0cluster?register="$mac" | cut -d',' -f 3 | cut -d':' -f2 | grep -o "[0-9]*")" 
-  echo "$clusterNumber" > /clusterNumber
-  
-  result="$(ps all | grep "..:[0-9][0-9] ./minerd" -o)"
-  case "$result" in
-    
-    #is not running
-    "")
-      echo 0 >/sys/class/leds/red_led/brightness
-      #echo "zero length"
-      ./start.sh --workername $clusterNumber > /mining.log &
-      ;;
-    
-    #is running
-    *)
-      #echo "non zero len"
-      echo 255 >/sys/class/leds/red_led/brightness
-      ;;
 
-  esac
+  for ((i=1;i<=50;i++)); 
+  do 
+    clusterNumber="$(curl 172.24.1.1:1880/opi0cluster?register="$mac" | cut -d',' -f 3 | cut -d':' -f2 | grep -o "[0-9]*")" 
+    echo "$clusterNumber" > /clusterNumber
 
-  sleep 10
+    result="$(ps all | grep "..:[0-9][0-9] ./minerd" -o)"
+    case "$result" in
 
+      #is not running
+      "")
+        echo 0 >/sys/class/leds/red_led/brightness
+        #echo "zero length"
+        ./start.sh --workername $clusterNumber > /mining.log &
+        ;;
+
+      #is running
+      *)
+        #echo "non zero len"
+        echo 255 >/sys/class/leds/red_led/brightness
+        ;;
+
+    esac
+
+    sleep 12
+  done
+  reboot
 done
