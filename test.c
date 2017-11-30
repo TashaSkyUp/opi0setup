57f329a140e37c051649f1184b7faa0bdaaab72e
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-29 18:49:51 -0700

    Update test.c

--- test.c
+++ test.c
@@ -2,23 +2,23 @@
 
     int main(int argc, char **argv)
     {
-  	FILE *test_file;
-  	char buf[4096];
-  	if ( (test_file = fopen ("/var/log/Xorg.0.log", 
-"r")) != NULL) printf("%i",argc);
-    {
-      while (!feof (test_file))	for(int 
-i=1;i<argc;i++)
-	{ {
+  	FILE *myFile;
+  	char buf[4];
+        myFile = fopen ("/khash","r");
+  	if (myFile != NULL){
+	printf("%i",argc);
+        while (!feof (test_file)){
+		for(int i=1;i<argc;i++){ 
 	  fgets (buf, sizeof (buf), test_file); 
-printf("%s",argv[i]);
-	  puts (buf); }
-	}    }
+          printf("%s",argv[i]);
+	  puts (buf); 
+		}
+	}
+	}
     }
   else
     {
-      printf ("Error opening file: %s\n", strerror 
-(errno));
+      printf ("Error opening file: %s\n", strerror (errno));
       exit (EXIT_FAILURE);
     }
   return 0;
