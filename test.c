4835b7f3acaed50c9a1575ad82551c4298433f44
TashaSkyUp <TashaSkyUp@gmail.com>
2017-11-29 18:54:31 -0700

    Update test.c

--- test.c
+++ test.c
@@ -5,20 +5,24 @@
   	FILE *myFile;
   	char buf[4];
         myFile = fopen ("/khash","r");
+	    
   	if (myFile != NULL){
 	printf("%i",argc);
         while (!feof (myFile)){
-		for(int i=1;i<argc;i++){ 
+	
+	for(int i=1;i<argc;i++){ 
 	  fgets (buf, sizeof (buf), myFile); 
           printf("%s",argv[i]);
 	  puts (buf); 
-		}
+	}
+		
 	}
 	}
-    }
+    
   else
     {
       printf ("Error opening file: %s\n", strerror (errno));
       exit (EXIT_FAILURE);
     }
   return 0;
+}
