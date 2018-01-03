6987d8d704608a4c10f9aab4c22fb58706c9f158
TashaSkyUp <TashaSkyUp@gmail.com>
2018-01-03 02:11:51 -0700

    Update test_versions.js

--- LFS/test_versions.js
+++ LFS/test_versions.js
@@ -1,1 +1,20 @@
+//function example
+function sd(n){
+}
+//setup common vars
+var arg1 = process.argv[2];
+var arg2 = process.argv[3];
 
+// for pipe
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(data) {
+  process.stdout.write(data);
+});
+
+//regex as if statment example
+if(/(crap)*/.test("crap")){
+}
+
+console.log(arg1);
+console.log(arg2);
