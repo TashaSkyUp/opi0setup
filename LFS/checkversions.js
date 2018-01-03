c1a36cfa73ebad214fde4cce022b944e4a407b1a
TashaSkyUp <TashaSkyUp@gmail.com>
2018-01-03 03:53:26 -0700

    Update checkversions.js

--- LFS/checkversions.js
+++ LFS/checkversions.js
@@ -18,7 +18,8 @@ if(/(crap)*/.test("crap")){
 var x=require('./data/reqver.json');//or require('data');
 console.log(x["-v"]);
 
-const { execSync } = require('child_process.execSync');
+const execSync = require('child_process').execSync;
+const { exec } = require('child_process');
 for (var f in x){
   console.log(f);
   for (var c in x[f]){
