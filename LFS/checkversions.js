a467497b82dcf7c4cdf0c1f1343327f0b1246f2e
TashaSkyUp <TashaSkyUp@gmail.com>
2018-01-03 03:36:02 -0700

    Update checkversions.js

--- LFS/checkversions.js
+++ LFS/checkversions.js
@@ -31,8 +31,8 @@ for (var f in x){
        //console.log(`stderr: ${stderr}`);
        var info = stdout+stderr
        console.log(info);
-       found = /[0-9].[0-9]/.exec(info)
-       console.log("found: "+found);
+       found = /[0-9](.[0-9])*/.exec(info)
+       console.log("exec: "+c+", found: "+found);
  
     });
   }
