83b584a6a232c5bf0e0046cacd89e517dc8580be
TashaSkyUp <TashaSkyUp@gmail.com>
2018-01-03 02:20:21 -0700

    Update checkversions.js

--- LFS/checkversions.js
+++ LFS/checkversions.js
@@ -16,5 +16,17 @@ process.stdin.on('data', function(data) {
 if(/(crap)*/.test("crap")){
 }
 
+const { exec } = require('child_process');
+exec('cat *.js bad_file | wc -l', (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    return;
+  }
+
+  // the *entire* stdout and stderr (buffered)
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
+
 console.log(arg1);
 console.log(arg2);
